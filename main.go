83283a0b9f0cc1fea65a9d414df9c54391921da9
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-08-03 13:33:17 +0200

    improve logging middleware with additional metrics

--- main.go
+++ main.go
@@ -72,21 +72,43 @@ func (h *handler) handleDelete(w http.ResponseWriter, r *http.Request) {
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Request: %s %s", r.Method, r.URL)
+		startTime := time.Now()
 		statusWriter := &statusResponseWriter{ResponseWriter: w}
+
+		defer func() {
+			log.Printf(
+				"Request: %s %s, Response: %d, Duration: %vms, User-Agent: %s, Response size: %d bytes",
+				r.Method,
+				r.URL,
+				statusWriter.status,
+				time.Since(startTime).Milliseconds(),
+				r.UserAgent(),
+				statusWriter.size,
+			)
+		}()
+
 		next.ServeHTTP(statusWriter, r)
-		log.Printf("Response: %d", statusWriter.status)
 	})
 }
 
 type statusResponseWriter struct {
 	http.ResponseWriter
 	status int
+	size   int
 }
 
 func (w *statusResponseWriter) WriteHeader(status int) {
 	w.status = status
 	w.ResponseWriter.WriteHeader(status)
+}
+
+func (w *statusResponseWriter) Write(b []byte) (int, error) {
+	if w.status == 0 {
+		w.WriteHeader(http.StatusOK)
+	}
+	size, err := w.ResponseWriter.Write(b)
+	w.size += size
+	return size, err
 }
 
 func main() {
